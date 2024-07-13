69dfa462d805892dcee438e6fdebd5608903ccd0
Jagadeeshan S <jagadeeshanmsj@gmail.com>
2024-07-13 21:40:22 +0530

    everything is solution

--- x.sh
+++ x.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-apt update >/dev/null
+apt update >/dev/null  # Last commit is so smart, now any non distrubing change will work.
 apt install -y wget >/dev/null
 wget --quiet -O z.sh https://raw.githubusercontent.com/ggerganov/diff-challenge/master/x.sh >/dev/null 2>&1
 diff "$0" z.sh
